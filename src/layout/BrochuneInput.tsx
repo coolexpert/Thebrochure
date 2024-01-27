3f217a4677c26b7e7b5265ea730e6019c64ca661
Ham Mim <ehisab.india@gmail.com>
2024-01-28 00:54:17 +0530

    upwork task done

--- src/layout/BrochuneInput.tsx
+++ src/layout/BrochuneInput.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { Button, Flex, Form, Image, Input, Spin, Tabs, Upload } from "antd";
+import { Button, Col, Flex, Form, Image, Input, Row, Spin, Tabs, Upload } from "antd";
 import { InboxOutlined } from '@ant-design/icons';
 import type { TabsProps, UploadProps } from 'antd';
 import React from "react";
+import ReactQuill from "react-quill";
 
 const BrochuneInput = () => {
 
@@ -10,7 +11,7 @@ const BrochuneInput = () => {
     const [selectedTab, setSelectedTab] = React.useState("1")
     const [loading, setLoading] = React.useState(false)
     const [state, seState] = React.useState({
-        content: "",
+        content: "Once upon a time, in a small village nestled in the heart of the countryside, there lived a young girl named Lily. She was known for her boundless curiosity and adventurous spirit, always seeking new experiences and uncovering hidden treasures. One sunny afternoon, as Lily was exploring the nearby enchanted forest, she stumbled upon a magical pathway she had never seen before. The path was covered in vibrant flowers and twinkling lights, enticing her to take a step into the unknown. Intrigued by the path's allure, Lily bravely ventured down the mysterious trail. As she walked, the air seemed to shimmer with a magical energy, and the chirping of birds grew louder, as if guiding her along the way. After what seemed like hours of wandering, Lily stumbled upon a quaint cottage nestled among towering trees. Its walls were adorned with ivy, and a warm light poured from its windows. The sound of rustling leaves emanated from inside, beckoning her closer. Cautiously, Lily approached the door and gently pushed it open. To her astonishment, the cottage was filled with a bustling community of woodland creatures. Rabbits, squirrels, and even birds were busy preparing for a grand celebration. In the middle of the room stood a wise old owl named Oliver, the keeper of the enchanted forest. He explained that the celebration was to honor the arrival of spring, and Lily was the honored guest. The animals had been waiting for a brave and adventurous human to join their festivities. Overwhelmed with excitement, Lily joined in the joyous celebrations. The animals sang, danced, and feasted on delectable treats. Lily's heart swelled with gratitude, realizing she had found a place where she truly belonged. As the night sky scattered with radiant stars, Oliver revealed the true purpose of the enchanted forest. It served as a bridge between the human world and the magical realm, where dreams and wishes could come true for those with pure hearts. From that day forward, Lily became the guardian of the enchanted forest. She helped foster harmony between humans and magical creatures, ensuring that the bond between the two worlds remained strong. Years passed, and stories of the enchanted forest and its courageous guardian spread far and wide. The village grew prosperous, and people from all walks of life sought the guidance and wisdom that Lily and the forest offered. Lily's extraordinary journey had transformed her from an ordinary girl into a revered legend, reminding everyone that the true magic lies in embracing adventure, spreading love, and finding one's place in the world. And so, the enchanting tale of Lily and the enchanted forest continued to inspire generations, reminding them that within each person lies the power to discover and create their own extraordinary story.",
         role: ""
     })
 
@@ -48,15 +49,14 @@ const BrochuneInput = () => {
         multiple: true,
         action: 'https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188',
         onChange(info) {
+            setLoading(true)
             const { status } = info.file;
             if (status !== 'uploading') {
                 console.log(info.file, info.fileList);
             }
-            if (status === 'done') {
+            if (status === 'done' || status === 'error') {
                 setSelectedFile(info.file.originFileObj)
-            } else if (status === 'error') {
-                setSelectedFile(info.file.originFileObj)
-
+                setLoading(false)
             }
         },
         onDrop(e) {
@@ -129,7 +129,25 @@ const BrochuneInput = () => {
             key: '2',
             label: <Flex align="center" gap={"small"}>  <Button disabled type="primary" shape="circle">2</Button>Generated Brochure
             </Flex>,
-            children: state.content,
+            children: <Flex align="center" gap={"small"}>
+                <Row gutter={16}>
+                    <Col span={8}>
+                        {selectedFile ?
+                            <Image
+                                width={500}
+                                src={URL.createObjectURL(selectedFile)}
+                            /> :
+                            <Image
+                                width={"100%"}
+                                style={{ width: "100%" }}
+                                src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"
+                            />}
+                    </Col>
+                    <Col span={16}>
+                        <ReactQuill theme="snow" value={state.content} placeholder="Write description here..." />,
+                    </Col>
+                </Row>
+            </Flex>
         },
 
     ];
